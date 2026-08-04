91338416fd7f307441f7bef2680f024f948c89c3
OBZESSED-WEB <thriftyy2.0@gmail.com>
2026-08-04 23:35:51 +0200

    Update Supabase URL to remove endpoint path

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://wluyakaqrvvzqvfxnmps.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://wluyakaqrvvzqvfxnmps.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_O5kw4jqiQE7RAegQnayg2g_Zu6OpoSS";
